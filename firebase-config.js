f1b562eca7679a2f8d45b9a578979a08f3695c33
markcf12-max <markcf12@gmail.com>
2026-07-20 19:03:12 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwEhTcIAJkwLoLCUh4eqEY20KsbenmXrQ",
+  authDomain: "pldtqamanagement.firebaseapp.com",
+  projectId: "pldtqamanagement",
+  storageBucket: "pldtqamanagement.firebasestorage.app",
+  messagingSenderId: "935852786747",
+  appId: "1:935852786747:web:08fd0e98362aceb805081e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export instances to be consumed by script.js
+export const auth = getAuth(app);
+export const db = getFirestore(app);
